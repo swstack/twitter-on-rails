c064235512a152ab3602efe6422b327967783563
Stephen Stack <stephen.stack@etherios.com>
2014-06-09 19:05:03 -0500

    Mutual-friends: Finish implementation for mutual friends (intersection)

--- app/assets/javascripts/mutualfollowers.js
+++ app/assets/javascripts/mutualfollowers.js
@@ -1,12 +1,28 @@
-function findMutualFollowersCallback(data) {
-    console.log(data)
+spinner = null;
+
+
+function startLoading () {
+    target = document.getElementById('loading-overlay');
+    spinner.spin(target);
+    $('#loading-overlay').addClass('loading-overlay')
 }
 
+function stopLoading () {
+    spinner.stop();
+    $('#loading-overlay').removeClass('loading-overlay')
+}
+
+function findMutualFollowersCallback(data) {
+    var template = Handlebars.compile($('#dyn-followers').html());
+    $("#user-followers-body").html(template({followers: data.user_followers}));
+    $("#mutual-followers-body").html(template({followers: data.mutual_followers}));
+    $("#target-followers-body").html(template({followers: data.target_followers}));
+    stopLoading();
+}
 
 function findMutualFollowersErrback(err) {
-    console.log(err)
+    stopLoading();
 }
-
 
 $(document).ready(function () {
     $('#tt-in').tooltip({title: 'Enter Twitter Screen name'})
@@ -18,10 +34,32 @@ $(document).ready(function () {
                 method: "GET",
                 url: "/mutualfollowers/find?screen_name=" + target_screenname.val(),
                 success: findMutualFollowersCallback,
+                error: findMutualFollowersErrback,
                 dataType: "json"
             })
+            startLoading();
         }
     });
+
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 10, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+    spinner = new Spinner(opts);
 
 });
 
--- app/controllers/mutualfollowers_controller.rb
+++ app/controllers/mutualfollowers_controller.rb
@@ -1,32 +1,85 @@
 class MutualfollowersController < ApplicationController
+
+  #-----------------------------------------------------------------------------
+  # Public (routes)
+  #-----------------------------------------------------------------------------
   def index
-    # Make sure the user is logged in before rendering this page
     if not is_logged_in
       redirect_to('/')
     end
 
-    @user_followers = _get_followers_for_screen_name(session[:screen_name])
     @user_screen_name = session[:screen_name]
   end
 
   def find
-    screen_name = params[:screen_name]
-    if screen_name != ''
-      followers = _get_followers_for_screen_name(screen_name)
-      @target_followers = followers
-      @mutual_followers = @user_followers & @target_followers
+    if not is_logged_in
+      redirect_to('/')
     end
 
-    render json: @product
+    response = Hash.new
+    screen_name = params[:screen_name]
+    if screen_name != ''
+
+      # find logged in users followers
+      user_followers = _make_users_hash(_get_followers_for_screen_name(session[:screen_name]))
+
+      # find followers for target user
+      target_followers = _make_users_hash(_get_followers_for_screen_name(screen_name))
+
+      # find mutual followers
+      mutual_followers = _intersect_users(user_followers, target_followers)
+
+      response[:user_followers] = user_followers
+      response[:target_followers] = target_followers
+      response[:mutual_followers] = mutual_followers
+
+    end
+
+    render json: response
   end
 
+  #-----------------------------------------------------------------------------
+  # Private Methods
+  #-----------------------------------------------------------------------------
   private
 
+  def _make_users_hash(users_array)
+    # This is a function that takes an array of `Twitter.User` objects and
+    # returns a hash of key/value pairs in the form of:
+    #         {
+    #              <uid>: {
+    #                       screen_name: <screen_name>,
+    #                       profile_url: <profile_url>,
+    #                     }
+    #         }
+
+    hashed_users = Hash.new
+    users_array.each { |user|
+      hashed_user = Hash.new
+      hashed_user[:screen_name] = user.attrs[:screen_name]
+      hashed_user[:profile_url] = 'https://twitter.com/%s' % user.attrs[:screen_name]
+      hashed_users[user.attrs[:id_str]] = hashed_user
+    }
+    return hashed_users
+  end
+
+  def _intersect_users(users_hash_1, users_hash_2)
+    # Given two users hashes (see: _make_users_hash) return a similar
+    # hash of their intersection based on the keys
+
+    keys_intersection = users_hash_1.keys & users_hash_2.keys
+    intersection = Hash.new
+    keys_intersection.each { |key|
+      intersection[key] = users_hash_1[key]
+    }
+    return intersection
+  end
+
   def _get_followers_for_screen_name(screen_name)
-    num_users = 100  # TODO: Handle more than 100 users
     client = get_twitter_client(session['user_token'], session['user_secret'])
     followers = []
-    following_ids = client.friend_ids(count=num_users, screen_name=screen_name)
+    following_ids = client.friend_ids(screen_name=screen_name)
     return client.users(user_id=following_ids.attrs[:ids])
   end
+
 end
